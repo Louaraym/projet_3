39fbf270be7f013750eec769f24f560c7705b66a
Raymond <louaraym@gmail.com>
2018-10-29 14:49:23 +0100

    Ajout des fichiers frontend.php et backend.php au sous dossier controleur et mise à jour du template dans le sous dossier frontend

---
+++ view/frontend/template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+    </head>   
+    <body>
+          <header>
+            <h1>Billet simple pour l'Alaska</h1>
+          </header>
+          <?= $content ?>
+          <footer></footer>
+    </body>
+</html>
